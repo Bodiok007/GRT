06cb96d63c5effb54b7414bd8dae241562c9d639
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-01-15 01:20:19 +0200

    Added mapping configuration

---
+++ GRT/GRT.DAL/Configuration/MappingConfiguration/EntityMappingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRT.DAL.Configuration.MappingConfiguration
+{
+    public abstract class EntityMappingConfiguration<T> : IEntityMappingConfiguration<T> where T : class
+    {
+        public abstract void Map(EntityTypeBuilder<T> builder);
+
+        public void Map(ModelBuilder builder)
+        {
+            Map(builder.Entity<T>());
+        }
+    }
+}
---
+++ GRT/GRT.DAL/Configuration/MappingConfiguration/IEntityMappingConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRT.DAL.Configuration.MappingConfiguration
+{
+    public interface IEntityMappingConfiguration
+    {
+        void Map(ModelBuilder builder);
+    }
+
+    public interface IEntityMappingConfiguration<T> : IEntityMappingConfiguration where T : class
+    {
+        void Map(EntityTypeBuilder<T> builder);
+    }
+}
