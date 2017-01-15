0f3c53c59651fc64280a5c864e6ae35db5e9cd31
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-01-15 15:19:09 +0200

    Added Local extenion for DbSet

---
+++ GRT/GRT.DAL/Contexts/Exentions/DbSetExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.Infrastructure;
+
+namespace GRT.DAL.Contexts.Exentions
+{
+    public static class DbSetExtensions
+    {
+        public static IEnumerable<T> Local<T>(this DbSet<T> set)
+          where T : class
+        {
+
+            var infrastructure = (IInfrastructure<IServiceProvider>)set;
+
+            var context = (DbContext)infrastructure.Instance.GetService(typeof(DbContext));
+
+            return context.ChangeTracker.Entries<T>()
+              .Where(e => 
+                  e.State == EntityState.Added || 
+                  e.State == EntityState.Unchanged || 
+                  e.State == EntityState.Modified)
+              .Select(e => e.Entity);
+        }
+    }
+}
