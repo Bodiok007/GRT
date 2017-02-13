0d3bbc20ccc6d2287636dcde7eed23f60f406bcb
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-02-13 23:15:49 +0200

    Added permission repository

---
+++ GRT/GRT.DAL/Repositories/EF/UserProject/PermissionRepository.cs
@@ -0,0 +1,36 @@
+﻿using GRT.DAL.Models.UserProject;
+using GRT.DAL.Repositories.Interfaces.UserProject;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using GRT.DAL.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GRT.DAL.Repositories.EF.UserProject
+{
+    public sealed class PermissionRepository 
+        : BaseRepository<PermissionDal>,
+          IPermissionRepository<PermissionDal>
+    {
+        public PermissionRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+
+        public IQueryable<PermissionDal> Get(
+            Expression<Func<PermissionDal, bool>> filter = null, 
+            Func<IQueryable<PermissionDal>, IOrderedQueryable<PermissionDal>> orderBy = null, 
+            string includeProperties = null)
+        {
+            var permissions = base.GetByCondition(filter, orderBy, includeProperties);
+
+            return permissions;
+        }
+
+        public PermissionDal GetById(object id)
+        {
+            var permission = _dbSet.Find();
+
+            return permission;
+        }
+    }
+}
---
+++ GRT/GRT.DAL/Repositories/Interfaces/UserProject/IPermissionRepository.cs
@@ -0,0 +1,10 @@
+﻿using GRT.DAL.Repositories.Interfaces.Operations;
+
+namespace GRT.DAL.Repositories.Interfaces.UserProject
+{
+    public interface IPermissionRepository<TEntity>
+        : IGetRepositoryOparation<TEntity>,
+          IGetByIdRepositoryOperation<TEntity> where TEntity : class
+    {
+    }
+}
