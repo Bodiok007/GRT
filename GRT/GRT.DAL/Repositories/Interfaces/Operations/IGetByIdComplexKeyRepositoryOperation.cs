d62b7d31f122cb891256fdbab1945574f356e51c
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-02-14 22:51:53 +0200

    Added IGetByIdComplexKeyRepositoryOperationinterface interface

---
+++ GRT/GRT.DAL/Repositories/Interfaces/Operations/IGetByIdComplexKeyRepositoryOperation.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace GRT.DAL.Repositories.Interfaces.Operations
+{
+    interface IGetByIdComplexKeyRepositoryOperation<TEntity> where TEntity : class
+    {
+        TEntity GetById(params object[] keys);
+    }
+}
