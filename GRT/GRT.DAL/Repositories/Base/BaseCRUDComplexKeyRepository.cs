59fb761851b32756165f9fbf5ff78e5ad5deb70e
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-02-17 09:00:15 +0200

    Created BaseCRUDRepository and BaseCRUDComplexKeyRepository

---
+++ GRT/GRT.DAL/Repositories/Base/BaseCRUDComplexKeyRepository.cs
@@ -0,0 +1,59 @@
+﻿using GRT.DAL.Repositories.Interfaces.Operations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace GRT.DAL.Repositories.Base
+{
+    public class BaseCRUDComplexKeyRepository<TEntity>
+        : BaseRepository<TEntity>,
+          IGetRepositoryOparation<TEntity>,
+          IGetByIdComplexKeyRepositoryOperation<TEntity>,
+          IUpdateRepositoryOparation<TEntity>,
+          IAddRepositoryOparation<TEntity>,
+          IDeleteRepositoryOperation<TEntity> where TEntity : class
+    {
+        public BaseCRUDComplexKeyRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+
+        public void Add(TEntity item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public void Delete(TEntity item)
+        {
+            if (_dbContext.Entry(item).State == EntityState.Detached)
+            {
+                _dbSet.Attach(item);
+            }
+
+            _dbSet.Remove(item);
+        }
+
+        public IQueryable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null)
+        {
+            var items = base.GetByCondition(filter, orderBy, includeProperties);
+
+            return items;
+        }
+
+        public TEntity GetById(params object[] keys)
+        {
+            var item = _dbSet.Find(keys);
+
+            return item;
+        }
+
+        public void Update(TEntity item)
+        {
+            _dbSet.Attach(item);
+            _dbContext.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
---
+++ GRT/GRT.DAL/Repositories/Base/BaseCRUDRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using GRT.DAL.Repositories.Interfaces.Operations;
+using Microsoft.EntityFrameworkCore;
+
+namespace GRT.DAL.Repositories.Base
+{
+    public class BaseCRUDRepository<TEntity, TKey>
+        : BaseRepository<TEntity>, 
+          IGetRepositoryOparation<TEntity>,
+          IGetByIdRepositoryOperation<TEntity, TKey>,
+          IUpdateRepositoryOparation<TEntity>,
+          IAddRepositoryOparation<TEntity>,
+          IDeleteRepositoryOperation<TEntity> where TEntity : class
+    {
+        public BaseCRUDRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+
+        public void Add(TEntity item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public void Delete(TEntity item)
+        {
+            if (_dbContext.Entry(item).State == EntityState.Detached)
+            {
+                _dbSet.Attach(item);
+            }
+
+            _dbSet.Remove(item);
+        }
+
+        public IQueryable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = null)
+        {
+            var items = base.GetByCondition(filter, orderBy, includeProperties);
+
+            return items;
+        }
+
+        public TEntity GetById(TKey id)
+        {
+            var item = _dbSet.Find(id);
+
+            return item;
+        }
+
+        public void Update(TEntity item)
+        {
+            _dbSet.Attach(item);
+            _dbContext.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
