9fa6bfe89885e9ee7cd1cfcf0b5fabde5da26bfe
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-03-08 23:34:12 +0200

    Added mapper config

---
+++ GRT/GRT.BLL/Mappers/AutoMapperConfig.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using GRT.DAL.Models.Tokens;
+using GRT.DAL.Models.UserProject;
+using GRT.Models.Tokens;
+using GRT.Models.UserProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRT.BLL.Mappers
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Configure()
+        {
+            /*AutoMapper.Mapper.CreateMap<User, UserDal>()
+                .ForMember(dest => dest.Author,
+                           opts => opts.MapFrom(src => src.Author.Name));*/
+
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<User, UserDal>();
+                cfg.CreateMap<UserDal, User>();
+                cfg.CreateMap<TokenDal, Token>();
+                cfg.CreateMap<Token, TokenDal>();
+            });
+
+            /*Mapper.Initialize(cfg => {
+                cfg.CreateMap<User, UserDal>();
+                cfg.CreateMap<UserDal, User>();
+            });*/
+
+            //IMapper mapper = config.CreateMapper();
+            /*var source = new AbcEditViewModel();
+            var dest = mapper.Map<AbcEditViewModel, Abct>(source);*/
+
+            return config.CreateMapper();
+        }
+    }
+}
