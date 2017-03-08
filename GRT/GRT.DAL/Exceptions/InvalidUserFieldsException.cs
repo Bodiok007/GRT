9fe1bd851165dc31fd5a8d0c842b2f01d411053f
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-03-08 23:30:26 +0200

    Added InvalidUserFieldsException

---
+++ GRT/GRT.DAL/Exceptions/InvalidUserFieldsException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRT.BLL.Exceptions
+{
+    public class InvalidUserFieldsException : Exception
+    {
+        public InvalidUserFieldsException(String message = _defaultMessage)
+            : base(message)
+        {
+
+        }
+
+        private const String _defaultMessage = "Invalid user fields!";
+    }
+}
