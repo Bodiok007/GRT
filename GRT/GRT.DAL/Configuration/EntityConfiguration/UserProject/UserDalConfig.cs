65b0406dee0b0e1dc088a005e808382d074c5619
Bohdan Hrybach <bodiahrubach@mail.ru>
2017-03-08 23:31:07 +0200

    Configured UserDal fields like unique

---
+++ GRT/GRT.DAL/Configuration/EntityConfiguration/UserProject/UserDalConfig.cs
@@ -0,0 +1,19 @@
+﻿using GRT.DAL.Configuration.MappingConfiguration;
+using GRT.DAL.Models.UserProject;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRT.DAL.Configuration.EntityConfiguration.UserProject
+{
+    public sealed class UserDalConfig : EntityMappingConfiguration<UserDal>
+    {
+        public override void Map(EntityTypeBuilder<UserDal> builder)
+        {
+            builder.HasIndex(user => user.Login).IsUnique();
+            builder.HasIndex(user => user.Email).IsUnique();
+        }
+    }
+}
